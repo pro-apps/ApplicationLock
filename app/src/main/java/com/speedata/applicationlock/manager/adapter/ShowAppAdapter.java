96b892aeba2283888cc425e93ad0b32f9439b74b
Reginer <282921012@qq.com>
2017-01-06 12:19:10 +0800

    应用显示fragment

---
+++ app/src/main/java/com/speedata/applicationlock/manager/adapter/ShowAppAdapter.java
@@ -0,0 +1,53 @@
+package com.speedata.applicationlock.manager.adapter;
+
+import android.content.Context;
+
+import com.speedata.applicationlock.R;
+import com.speedata.applicationlock.bean.AppInfo;
+import com.speedata.applicationlock.common.ToolsCommon;
+
+import java.util.List;
+
+import xyz.reginer.baseadapter.BaseAdapterHelper;
+import xyz.reginer.baseadapter.CommonRvAdapter;
+
+/**
+ * ----------Dragon be here!----------/
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃神兽保佑
+ * 　　　　┃　　　┃代码无BUG！
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━神兽出没━━━━━━
+ *
+ * @author Reginer on  2016/12/15 11:56.
+ *         Description:设置显示应用Adapter
+ */
+public class ShowAppAdapter extends CommonRvAdapter<AppInfo> {
+    private Context mContext;
+
+    public ShowAppAdapter(Context context, int layoutResId, List<AppInfo> data) {
+        super(context, layoutResId, data);
+        mContext = context;
+    }
+
+    @Override
+    public void convert(BaseAdapterHelper helper, AppInfo item, int position) {
+        helper.setImageDrawable(R.id.app_icon, ToolsCommon.getAppIcon(mContext, item.getAppPkg(), item.getActName()));
+        helper.setText(R.id.app_name, item.getAppLabel());
+        helper.setText(R.id.app_size, item.getAppPkg());
+        helper.setChecked(R.id.hide, !item.isHide());
+    }
+}
--- app/src/main/java/com/speedata/applicationlock/manager/fragment/ShowFragment.java
+++ app/src/main/java/com/speedata/applicationlock/manager/fragment/ShowFragment.java
@@ -2,19 +2,56 @@ package com.speedata.applicationlock.manager.fragment;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.speedata.applicationlock.R;
 import com.speedata.applicationlock.base.BaseFragment;
+import com.speedata.applicationlock.bean.AppChanged;
+import com.speedata.applicationlock.bean.AppInfo;
+import com.speedata.applicationlock.common.DbCommon;
+import com.speedata.applicationlock.manager.adapter.ShowAppAdapter;
+
+import org.greenrobot.eventbus.EventBus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import xyz.reginer.baseadapter.CommonRvAdapter;
 
 /**
- * A simple {@link Fragment} subclass.
+ * ----------Dragon be here!----------/
+ * 　　　┏┓　　　┏┓
+ * 　　┏┛┻━━━┛┻┓
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　━　　　┃
+ * 　　┃　┳┛　┗┳　┃
+ * 　　┃　　　　　　　┃
+ * 　　┃　　　┻　　　┃
+ * 　　┃　　　　　　　┃
+ * 　　┗━┓　　　┏━┛
+ * 　　　　┃　　　┃神兽保佑
+ * 　　　　┃　　　┃代码无BUG！
+ * 　　　　┃　　　┗━━━┓
+ * 　　　　┃　　　　　　　┣┓
+ * 　　　　┃　　　　　　　┏┛
+ * 　　　　┗┓┓┏━┳┓┏┛
+ * 　　　　　┃┫┫　┃┫┫
+ * 　　　　　┗┻┛　┗┻┛
+ * ━━━━━━神兽出没━━━━━━
+ * 创   建:Reginer in  2017/1/6 11:04.
+ * 联系方式:QQ:282921012
+ * 功能描述:显示应用
  */
-public class ShowFragment extends BaseFragment {
+public class ShowFragment extends BaseFragment implements View.OnClickListener, CommonRvAdapter.OnItemClickListener {
 
+    private ShowAppAdapter mAdapter;
+    private List<AppInfo> mCanShowAppList;
 
     public ShowFragment() {
         // Required empty public constructor
@@ -24,8 +61,42 @@ public class ShowFragment extends BaseFragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_show, container, false);
+       View mView = inflater.inflate(R.layout.fragment_show, container, false);
+        initView(mView);
+        return mView;
     }
 
+    private void initView(View mView) {
+        mCanShowAppList = new ArrayList<>();
+        mCanShowAppList.addAll(DbCommon.queryAppList(true));
+        mAdapter = new ShowAppAdapter(getActivity(), R.layout.view_hide_app_item_layout, mCanShowAppList);
+        RecyclerView mRvContent = (RecyclerView) mView.findViewById(R.id.rv_content);
+        mRvContent.setLayoutManager(new LinearLayoutManager(getActivity()));
+        mRvContent.addItemDecoration(new DividerItemDecoration(getActivity(), LinearLayoutManager.VERTICAL));
+        mRvContent.setAdapter(mAdapter);
+        Button mBtnSave = (Button) mView.findViewById(R.id.btn_save);
+        mBtnSave.setOnClickListener(this);
+        mAdapter.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_save:
+                save();
+                break;
+        }
+    }
+
+    private void save() {
+        DbCommon.saveAppList(mCanShowAppList);
+        EventBus.getDefault().post(new AppChanged(true));
+    }
+
+
+    @Override
+    public void onItemClick(RecyclerView.ViewHolder viewHolder, View view, int position) {
+        mCanShowAppList.get(position).setHide(!mCanShowAppList.get(position).isHide());
+        mAdapter.notifyDataSetChanged();
+    }
 }
