8383b6bd20574c31a94308308dd6443ff6ab6563
Reginer <282921012@qq.com>
2017-01-16 14:48:05 +0800

    选项全部入口

--- app/src/main/java/com/speedata/applicationlock/options/fragment/NotificationFragment.java
+++ app/src/main/java/com/speedata/applicationlock/options/fragment/NotificationFragment.java
@@ -2,10 +2,13 @@ package com.speedata.applicationlock.options.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.TextView;
 
 import com.speedata.applicationlock.R;
 import com.speedata.applicationlock.options.OptionsActivity;
@@ -30,13 +33,15 @@ import com.speedata.applicationlock.options.OptionsActivity;
  * 　　　　　┃┫┫　┃┫┫
  * 　　　　　┗┻┛　┗┻┛
  * ━━━━━━神兽出没━━━━━━
- *
+ * <p>
  * 创   建:Reginer in  2017/1/13 12:23.
  * 联系方式:QQ:282921012
  * 功能描述:通知栏配置Fragment
  */
-public class NotificationFragment extends Fragment {
+public class NotificationFragment extends Fragment implements View.OnClickListener {
 
+    private CheckBox mShowBox;
+    private CheckBox mEnableBox;
 
     public NotificationFragment() {
         // Required empty public constructor
@@ -49,6 +54,24 @@ public class NotificationFragment extends Fragment {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_notification, container, false);
     }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        mShowBox = (CheckBox) view.findViewById(R.id.cb_is_allow);
+        mEnableBox = (CheckBox) view.findViewById(R.id.cb_is_enable);
+        TextView mShowNotificationTitle = (TextView) view.findViewById(R.id.tv_title);
+        TextView mShowNotificationSummary = (TextView) view.findViewById(R.id.tv_summary);
+        mShowNotificationTitle.setText(R.string.show_notification);
+        mShowNotificationSummary.setText(R.string.to_show_notification);
+        mShowBox.setVisibility(View.VISIBLE);
+
+
+        view.findViewById(R.id.rl_show_notification).setOnClickListener(this);
+        view.findViewById(R.id.rl_enable_notification).setOnClickListener(this);
+    }
+
     @Override
     public void onStart() {
         super.onStart();
@@ -61,4 +84,24 @@ public class NotificationFragment extends Fragment {
         ((OptionsActivity) getActivity()).mToolbar.setTitle(R.string.options);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.rl_show_notification:
+                showNotification();
+                break;
+            case R.id.rl_enable_notification:
+                enableNotification();
+                break;
+
+        }
+    }
+
+    private void enableNotification() {
+        mEnableBox.setChecked(!mEnableBox.isChecked());
+    }
+
+    private void showNotification() {
+        mShowBox.setChecked(!mShowBox.isChecked());
+    }
 }
